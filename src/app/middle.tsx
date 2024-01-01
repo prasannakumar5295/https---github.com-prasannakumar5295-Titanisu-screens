a0462a1cb04459bb98a5007b9757f74f44789e55
prasannakumar <prasannakumar2c7@gmail.com>
2024-01-01 09:58:20 +0530

    init

---
+++ src/app/middle.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+
+const Middle = () => {
+  return (
+    <div className="">
+    
+      <div className="text-center mt-1">
+        <div className="p-2 relative flex place-items-center left-40 before:absolute before:h-[100px] ...">
+          <h3>I'm BUzz,</h3>
+        </div>
+        <p className="text-sm text-gray-700">
+          your creative and helpful collaborator. I have limitations and <br /> won't always get it right, but your feedback helps me<br /> improve.
+        </p>
+        <p className="p-4 text-sm text-green-500">
+          First, read my guide to learn how to get the most out of me
+        </p>
+      </div>
+
+      {/* <div className="grid grid-cols-3 gap-8 mt-4">
+        <div>
+          <p className="text-center text-gray-700 p-4 font-bold"> Admissions</p>
+          <div className="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p className="font-sm text-gray-700 dark:text-gray-400">BU has been educating students<br /> of all kinds for more than<br /> 150 years</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-center text-gray-700 p-4 font-bold">Academics</p>
+          <div className="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p className="font-normal text-gray-700 dark:text-gray-400">Pick your odyssey<br /> Explore BU's 300+ programs 0f<br /> study</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-center text-gray-700 p-4 font-bold">Research</p>
+          <div className="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p className="font-normal text-gray-700 dark:text-gray-400">The research done at BU's crosses<br /> disciplines,defies convention and<br /> transcend borders</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-center text-gray-700 p-4 font-bold">Global Identity</p>
+          <div className="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p className="font-normal text-gray-700 dark:text-gray-400">Changing the world is part of our <br />identity.Every year,we field<br />thousands of students and<br />researchers overseas.</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-center text-gray-700 p-4 font-bold">Campus Life</p>
+          <div className="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p className="font-normal text-gray-700 dark:text-gray-400">Our slice of boston is a mosiac like <br />no other; a swirl languages<br /> cultures and religions.</p>
+          </div>
+        </div>
+        <div>
+          <p className="text-center text-gray-700 p-4 font-bold">About BU</p>
+          <div className="block max-w-lg p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+            <p className="font-normal text-gray-700 dark:text-gray-400"><br /> Boston is no small operation with <br />over 37000 students from more<br />than 140 countries,over 10,000<br />faculty and staff,17 colleges.</p>
+          </div>
+        </div>
+      </div> */}
+    </div>
+  );
+};
+
+export default Middle;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,120 +1,257 @@
 "use client";
 import Sidebar from './sidebar';
 import { useState } from 'react';
-import chatbotData from './caht-botdata.json';
-import Image from 'next/image'
-
+import chatbotData from './chat-botdata.json';
+import Middle from './middle'
 export default function Home() {
   const [question, setQuestion] = useState('');
   const [chatHistory, setChatHistory] = useState([]);
+  const [activeTab, setActiveTab] = useState('');
+  const [isSearching, setIsSearching] = useState(false);
 
   const handleSearch = () => {
     const matchingEntries = chatbotData.pairs.filter((entry) =>
       entry.question.toLowerCase().includes(question.toLowerCase())
     );
 
-    // Update the chat history with user's question and chatbot's answer
-    setChatHistory([...chatHistory, { type: 'user', text: question }, ...matchingEntries[0]?.answer ? [{ type: 'bot', text: matchingEntries[0].answer }] : []]);
+    const updatedHistory = [
+      ...chatHistory,
+      {
+        type: 'user',
+        text: question,
+      },
+      ...matchingEntries[0]?.answer
+        ? [
+            {
+              type: 'bot',
+              text: matchingEntries[0].answer,
+              suggestions: matchingEntries[0].suggestions,
+              videos: matchingEntries[0].videos,
+              links: matchingEntries[0].links,
+            },
+          ]
+        : [],
+    ];
+
+    setChatHistory(updatedHistory);
     setQuestion('');
+    setIsSearching(true);
+
   };
 
-  console.log("chatHistory", chatHistory);
+  const renderTabContent = () => {
+  switch (activeTab) {
+    case 'suggestions':
+      return (
+        <div className="space-y-4">
+          {chatHistory
+            .filter((chatItem) => chatItem.type === 'bot' && chatItem.suggestions)
+            .map((chatItem, index) => (
+              <div key={index}>
+                {chatItem.suggestions.map((suggestion, innerIndex) => (
+                  <div
+                    key={innerIndex}
+                    className="bg-white text-gray-900 px-2 py-2 rounded-md cursor-pointer border border-gray-300"
+                  >
+                    {suggestion}
+                  </div>
+                ))}
+              </div>
+            ))}
+        </div>
+      );
+    case 'videos':
+      return (
+        <div className="space-y-8 rounded-lg p-4 bg-200">
+          {chatHistory
+            .filter((chatItem) => chatItem.type === 'bot' && chatItem.videos)
+            .map((chatItem, index) => (
+              <div key={index}>
+                {chatItem.videos.map((video, innerIndex) => (
+                  <div
+                    key={innerIndex}
+                    className="bg-white text-gray-900  rounded-md cursor-pointer border border-gray-300"
+                  >
+                    {video.title} - {video.url}
+                  </div>
+                ))}
+              </div>
+            ))}
+        </div>
+      );
+    case 'links':
+      return (
+        <div className="space-y-4 rounded-xl">
+          {chatHistory
+            .filter((chatItem) => chatItem.type === 'bot' && chatItem.links)
+            .map((chatItem, index) => (
+              <div key={index}>
+                {chatItem.links.map((link, innerIndex) => (
+                  <div
+                    key={innerIndex}
+                    className="bg-white text-gray-900 p-4 rounded-md cursor-pointer border border-gray-300"
+                  >
+                    {link}
+                  </div>
+                ))}
+              </div>
+            ))}
+        </div>
+      );
+    default:
+      return null;
+  }
+};
 
+  console.log("chat",chatHistory)
   return (
     <div>
       <Sidebar />
       <main className="flex min-h-screen flex-col items-center justify-between p-40">
-      <div className="flex flex-col space-y-4 max-w-md w-full mx-auto">
-      <div >
-           <button style={{ "color": "#3f48cc", "borderBlockColor": "#3f48cc" }} className="fixed top-4 right-4 bg-white text-sm-blue px-7 py-2 rounded-full cursor-pointer">
-             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
-               <svg
-                 style={{ "color": "#3f48cc" }}
-                 className="w-[16px] h-[16px] text-gradient dark:text-white"
-                 aria-hidden="true"
-                 xmlns="http://www.w3.org/2000/svg"
-                 fill="currentColor"
-                 viewBox="0 0 22 21"
-               >
-                <path d="M12.356 5.435 1.938 16.384a.5.5 0 0 0 .018.707l2.9 2.757a.5.5 0 0 0 .706-.018L15.978 8.882l-3.622-3.447Zm7.681-.819a.5.5 0 0 0-.018-.706l-2.9-2.757a.5.5 0 0 0-.707.017l-2.68 2.817 3.622 3.446 2.683-2.817Zm-2.89 12.233-1 .025-.024-1a1 1 0 1 0-2 .05l.025 1-1 .024a1 1 0 0 0 .05 2l1-.025.025 1a1 1 0 1 0 2-.05l-.025-1 1-.024a1 1 0 1 0-.05-2h-.001ZM2.953 9.2l.025 1a1 1 0 1 0 2-.05l-.025-1 1-.025a1 1 0 1 0-.05-2l-1 .025-.025-1a1 1 0 0 0-2 .049l.025 1-1 .025a1 1 0 0 0 .05 2l1-.024Zm15.07 2.626-2 .05.05 1.999 2-.05-.05-1.999ZM11.752.978l-2 .05.05 2 2-.05-.05-2Zm-2.95 2.075-2 .05.05 1.999 2-.05-.05-2ZM5.753 1.127l-1.999.05.05 2 1.999-.05-.05-2Zm15.194 7.625-2 .05.05 2 2-.05-.05-2Zm.125 4.998-2 .05.05 2 2-.05-.05-2Z" />
-               </svg>
-             </div>
-             New Chat
-           </button>
-         </div> 
-        
+        <div className="flex flex-col space-y-2 max-w-md w-full mx-auto">
+        {!isSearching && (
+          <div className="flex flex-col space-y-2 max-w-md w-full mx-auto">
+            <Middle />
+          </div>
+        )}
           {chatHistory.map((chatItem, index) => (
+            
             <div
               key={index}
-              className={`rounded-lg p-4 ${chatItem.type === 'user' ? 'bg-white text-black self-start' : 'bg-white text-gray-800 self-start'}`}
+              className={`rounded-lg p-4 ${
+                chatItem.type === 'user'
+                  ? 'bg-gray-280 text-black self-start'
+                  : 'bg-gray-300 text-black self-start'
+              }`}
             >
-              {/* Display the logo for bot responses */}
-              {chatItem.type === 'bot' && (
-                <div className="mb-2">
-                  <Image
-          
-            src="/logo.png"
-            alt="Next.js Logo"
-            width={100}
-            height={40}
-            
-            priority
-          />
-              
+              {chatItem.type === 'user' && (
+                <div className="flex items-center">
+                  <div className="relative w-9 h-9 overflow-hidden bg-gray-100 rounded-full dark:bg-blue-400 mr-2">
+                    <svg
+                      className="absolute w-12 h-12 text-gray-400 -left-1"
+                      fill="currentColor"
+                      viewBox="0 0 20 20"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                  </div>
+                  <h6>{chatItem.text}</h6>
                 </div>
               )}
 
-              {/* Display the answer text */}
-              {chatItem.text}
-            </div>
-          ))}
-        </div>
+              {chatItem.type === 'bot' && (
+                <div className="mb-2">
+                  <h6 className="font-extrabold text-xl">BOSTAN.AI</h6>
 
-        {/* <div className="flex items-center space-x-4 mt-8">
-          <input
-            type="text"
-            value={question}
-            onChange={(e) => setQuestion(e.target.value)}
-            placeholder="Ask a question..."
-            className="flex-1 p-2 border rounded-md"
-          />
-          <button onClick={handleSearch} className="bg-blue-500 text-white px-4 py-2 rounded-full">
-            Search
-          </button>
-        </div> */}
-         <div className="bg-white-500 p-4 text-black fixed  bottom-0 left-60  w-3/4 z-50">
-                <div className="flex items-center space-x-4 mt-8">
-          <input
-            type="text"
-            placeholder="Tell me what do you want?"
-            id="voice-search"
-            value={question}
-            onChange={(e) => setQuestion(e.target.value)}
-            className="bg-gray-50 border border-gray-300 text-gray-900 text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-4 rounded-3xl  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          />
-          <button
-            type="button"
-            className="absolute inset-y-0 right-0 flex items-center pr-6"
+                  {chatItem.text}
+                  {chatItem.suggestions && (
+                    <div className="bg-gray-300 p-4 rounded-lg border-black">
+                      <button
+                        className={`px-6 py-3 ${
+                          activeTab === 'suggestions'
+                            ? 'bg-white text-black rounded-xl'
+                            : 'bg-gray-300 text-black'
+                        }`}
+                        onClick={() => setActiveTab('suggestions')}
+                      >
+                        Suggestions
+                      </button>
+                      {chatItem.videos && (
+                        <button
+                          className={`px-6 py-3 ${
+                            activeTab === 'videos'
+                              ? 'bg-white text-black rounded-xl'
+                              : 'bg-gray-300 text-black'
+                          }`}
+                          onClick={() => setActiveTab('videos')}
+                        >
+                          Videos
+                        </button>
+                      )}
+                      {chatItem.links && (
+                        <button
+                          className={`px-6 py-3 ${
+                            activeTab === 'links'
+                              ? 'bg-white text-black rounded-xl'
+                             : 'bg-gray-300 text-black'
+                          }`}
+                          onClick={() => setActiveTab('links')}
+                        >
+                          Links
+                        </button>
+                      )}
+<button>
+                  <svg className="w-6 h-6 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2Zm-3 15H4.828a1 1 0 0 1 0-2h6.238a1 1 0 0 1 0 2Zm0-4H4.828a1 1 0 0 1 0-2h6.238a1 1 0 1 1 0 2Z" />
+                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                  </svg>
+                </button>
+                <button>
+
+                  <svg className="w-6 h-6 text-gray-500 dark:text-white bg-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                    <path d="M3 7H1a1 1 0 0 0-1 1v8a2 2 0 0 0 4 0V8a1 1 0 0 0-1-1Zm12.954 0H12l1.558-4.5a1.778 1.778 0 0 0-3.331-1.06A24.859 24.859 0 0 1 6 6.8v9.586h.114C8.223 16.969 11.015 18 13.6 18c1.4 0 1.592-.526 1.88-1.317l2.354-7A2 2 0 0 0 15.954 7Z" />
+                  </svg>
+                </button>
+                <button>
+
+                  <svg className="w-6 h-6 text-gray-500 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
+                    <path d="M11.955 2.117h-.114C9.732 1.535 6.941.5 4.356.5c-1.4 0-1.592.526-1.879 1.316l-2.355 7A2 2 0 0 0 2 11.5h3.956L4.4 16a1.779 1.779 0 0 0 3.332 1.061 24.8 24.8 0 0 1 4.226-5.36l-.003-9.584ZM15 11h2a1 1 0 0 0 1-1V2a2 2 0 1 0-4 0v8a1 1 0 0 0 1 1Z" />
+
+                  </svg>
+                </button>
+                    </div>
+                  )}
+                </div>
+              )}
+            </div>
             
-          >
-            <svg className="w-5 h-5 text-black dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 16 20">
-            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 7v3a5.006 5.006 0 0 1-5 5H6a5.006 5.006 0 0 1-5-5V7m7 9v3m-3 0h6M7 1h2a3 3 0 0 1 3 3v5a3 3 0 0 1-3 3H7a3 3 0 0 1-3-3V4a3 3 0 0 1 3-3Z" />
-          </svg>
-          </button>
+          ))}
+          <div className="mt-8">{renderTabContent()}</div>
         </div>
         
-          {/* <svg className="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-              <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3" />
-            </svg>
-        */}
-        <button onClick={handleSearch}>
-            <svg className="bg-gray-200 w-8 h-8 text-blue-500 dark:text-blue-500  rotate-90  rounded-full " aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20">
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9"/>
-    </svg>
-</button>
-</div>
-  </main>
+        <div className="bg-white-500 p-4 text-black fixed bottom-0 left-90 w-1/2 z-40">
+          <div className="flex items-center space-x-2 mt-8 relative">
+            <input
+              type="text"
+              placeholder="Tell me what do you want?"
+              id="voice-search"
+              value={question}
+              onChange={(e) => setQuestion(e.target.value)}
+              className="bg-white border border-blue-400 text-black text-sm focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-4 rounded-xl dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 pr-16" // Adjusted styling here
+              required
+            />
+
+            <button onClick={handleSearch}>
+              <svg
+                className="bg-blue-800 w-12 h-12 text-white dark:text-white rotate-90 rounded-full"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 18 20"
+              >
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="m9 17 8 2L9 1 1 19l8-2Zm0 0V9"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+
+        
+      </main>
     </div>
   );
 }
+
+
+
+
